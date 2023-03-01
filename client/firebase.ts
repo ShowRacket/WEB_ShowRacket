8d1e32b53a25dd977a099ad386487dde71e76fe9
Min-h-96 <asdf6256@gmail.com>
2023-03-01 15:15:19 +0900

    first commit

---
+++ client/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDP1yhYeCGWxp-z-SMg8RgDB5HRnp79Hyc",
+  authDomain: "squashracket-1a989.firebaseapp.com",
+  projectId: "squashracket-1a989",
+  storageBucket: "squashracket-1a989.appspot.com",
+  messagingSenderId: "509963603726",
+  appId: "1:509963603726:web:0dd96b9ad61944bee0115f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
